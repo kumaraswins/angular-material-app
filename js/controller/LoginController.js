067d1a3535f4364ba2ef6571a9059f9e642e751f
AswinKumar <kumaraswinswin@gmail.com>
2016-01-30 17:23:07 +0530

    Update LoginController.js

--- js/controller/LoginController.js
+++ js/controller/LoginController.js
@@ -2,7 +2,9 @@ angular.module('SampleAPP')
     .controller('LoginController', ['$scope','$state', '$http', '$location', 'DataService', LoginController]);
 
 function LoginController($scope,$state, $http, $location, DataService) {
-     
+     $scope.open = function(e){
+         e.preventDefault()
+     }
     $scope.login = function() {
         $state.go("dashboard");
     }
